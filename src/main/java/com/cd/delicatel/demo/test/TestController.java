2f13b6e2f08b8c1706c2d20052cad0703003d331
cold <zjchenliang@126.com>
2019-08-12 11:10:27 +0800

    初始化

---
+++ src/main/java/com/cd/delicatel/demo/test/TestController.java
@@ -0,0 +1,20 @@
+package com.cd.delicatel.demo.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author cold
+ * @Date 2019/8/12 10:45
+ **/
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/test")
+    public String test() {
+        System.out.println("WAWAWA!!!!!!");
+        return "success!!!!!!!!!!!!!!!";
+    }
+
+}
