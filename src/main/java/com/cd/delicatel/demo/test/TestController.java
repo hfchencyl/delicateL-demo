eb30e3e4a5972faf72aaf32bf1ab4a3d29de2f91
cold <zjchenliang@126.com>
2019-08-14 17:18:55 +0800

    1

--- src/main/java/com/cd/delicatel/demo/test/TestController.java
+++ src/main/java/com/cd/delicatel/demo/test/TestController.java
@@ -16,7 +16,7 @@ public class TestController {
     public String test() {
         System.out.println("WAWAWA!!!!!!");
         System.out.println("我多加了一行打印!!!!!!");
-        return "success!!!!!!!!!!!!!!!我卡卡，终于成功了！！！";
+        return "success!!!!!!!!!!!!!!!123我卡卡，终于成功了！！！";
     }
 
 }
